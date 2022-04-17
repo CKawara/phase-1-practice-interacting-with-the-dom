bd315282f99384cd910038345cbddb812e6ba435
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-17 15:16:30 +0300

    add manual count function

--- js/challenge.js
+++ js/challenge.js
@@ -1,11 +1,24 @@
 let countId;
+let num = document.getElementById('counter')
+
 
 function count(){
     countId = setInterval(addnum, 1000)
 }
 
 function addnum (){
-    let num = document.getElementById('counter')
     num.textContent = parseInt(num.textContent)+1
 }
+function manualCounter(){
+    let add = document.getElementById('plus')
+    let minus = document.getElementById('minus')
+    add.addEventListener('click', ()=>{
+        num.textContent = parseInt(num.textContent)+1
+    })
+    minus.addEventListener('click', ()=>{
+        if(num.textContent != 0)
+        num.textContent = parseInt(num.textContent)-1
+    })
+}
 count()
+manualCounter()
